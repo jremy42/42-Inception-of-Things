a3a7757c04247b62335b1a26d5f79dc845eb1e72
root <jonathan.prudent13@gmail.com>
2023-07-26 15:07:33 +0000

    modif config

--- p1/scripts/master.sh
+++ p1/scripts/master.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
 
-echo "[START] - install k3s"
+echo "[START] - launch instalation script"
 
 echo "[1] -disable swap"
 swapoff -a 
 
 echo "[2] -install utils"
 
-apt-get update --qq
-apt-get install -y --qq apt-transport-https curl
+apt-get update
+apt-get install -y apt-transport-https curl
 
-# curl -sfL https://get.k3s.io  |  INSTALL_K3S_EXEC="agent --node-ip 192.168.42.111"\
-# 	K3S_URL="https://192.168.42.110:6443"\
-# 	K3S_TOKEN="713705"\
-# 	K3S_KUBECONFIG_MODE="644" sh -
+
+#echo "[3] - install k3s"
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip 192.168.42.110" K3S_KUBECONFIG_MODE="644" sh -
+
+#echo "[4] - create alias"
+
+#echo "alias k='kubectl'" >> /etc/profile.d/00-aliases.sh
+
+echo "server as been setup successfuly"
