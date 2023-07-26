f84ecbdca7cb791b0b3b36bef6b868a9114a3711
jonathan <jonathan.prudent13@gmail.com>
2023-07-26 17:31:34 +0200

    add memory for start k3s

--- p1/scripts/master.sh
+++ p1/scripts/master.sh
@@ -15,8 +15,8 @@ apt-get install -y apt-transport-https curl
 #echo "[3] - install k3s"
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip 192.168.42.110" K3S_KUBECONFIG_MODE="644" sh -
 
-#echo "[4] - create alias"
+echo "[4] - create alias"
 
-#echo "alias k='kubectl'" >> /etc/profile.d/00-aliases.sh
+echo "alias k='kubectl'" >> /etc/profile.d/00-aliases.sh
 
 echo "server as been setup successfuly"
